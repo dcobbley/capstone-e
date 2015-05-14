6f2043222fa3c455c5a5d7947b86bff32983f639
Ruben Niculcea <ruben.niculcea@gmail.com>
2015-05-14 14:07:25 -0700

    Use node-execute to launch Firefox OS node modules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function (grunt) {
 	// Load task modules
 	grunt.loadNpmTasks('grunt-contrib-uglify');  // -- Minifies JS
 	grunt.loadNpmTasks('grunt-browserify');  // ------ Injects dependencies
+  grunt.loadNpmTasks('grunt-execute'); // Run node tasks
 
 	// Configure tasks
 	grunt.initConfig({
@@ -27,23 +28,29 @@ module.exports = function (grunt) {
 				src: 'ourlib.js',
 				dest: 'ourlib.min.js'
 			}
-		}
+		},
+
+    execute: {
+      target: {
+        src: 'node-firefox-scripts/launch-firefoxos.js'
+      }
+    }
 	});
 
 
-	/**
-	 * Rather than support a default task, "grunt"
-	 * will return a simple list of registered
-	 * grunt tasks, similar to a "--help" option.
-	*/
 	grunt.registerTask('default', function () {
-		console.log('\nSorry, no "default" grunt task.');
-		console.log('Registered tasks are:\n');
-		console.log('grunt build -- Create distributable from modules/main.');
-		console.log('grunt test -- Deploy and run all tests. Will check for device, then simulator.');
-		console.log('  opt: sim [version] -- Try to connect to a specific simulator.');
-		console.log('  opt: create -- Create new unit test.');
+    console.log('No opitions specified defaulting to push to any device then simulator');
+    grunt.task.run(['execute']);
 	});
+
+
+  grunt.registerTask('help', function () {
+    console.log('Registered tasks are:\n');
+    console.log('grunt build -- Create distributable from modules/main.');
+    console.log('grunt test -- Deploy and run all tests. Will check for device, then simulator.');
+    console.log('  opt: sim [version] -- Try to connect to a specific simulator.');
+    console.log('  opt: create -- Create new unit test.');
+  });
 
 
 	/**
@@ -72,136 +79,22 @@ module.exports = function (grunt) {
 			createQunitTest(grunt.option('create'));
 		}
 		else if (grunt.option('sim')) {
-			pushToVersion(grunt.option('sim'));
+      var simVersion = grunt.option('sim').toFixed(1);
+      grunt.config.set('execute.options.args', ['pushToVersion', simVersion]);
+      grunt.task.run(['execute']);
 		}
 		else {
-			pushToAnything();
+      grunt.config.set('execute.options.args', ['pushToAnything']);
+      grunt.task.run(['execute']);
 		}
 	});
 
-
-	/*---------------------------------------------------------------*/
-	/*---------------------- UTILITY FUNCTIONS ----------------------*/
-	/*---------------------------------------------------------------*/
-
-	var fs = require('fs');  // ----- Node's Filesystem module
-	var path = require('path');  // - Node's filepath module
-
-	// A boat-load of node-firefox libraries
-	var Promise = require('es6-promise').Promise;
-	var findDevices = require('node-firefox-find-devices');
-	var findSimulators = require('node-firefox-find-simulators');
-	var startSimulator = require('node-firefox-start-simulator');
-	var forwardPorts = require('node-firefox-forward-ports');
-	var connect = require('node-firefox-connect');
-	var launchApp = require('node-firefox-launch-app');
-	var pushApp = require('push-app');
-
-	// Where our app is located
-	var appPath = path.join(__dirname, 'testrunner');
-
-
-	/**
-	 * Push the app (unit tests) to a Firefix OS simulator of a
-	 * specific version. You will be notified if the specified
-	 * runtime does not exist. You can install runtimes via the
-	 * Firefox WebIDE.
-	 * If the provided version is "any", it will simply connect
-	 * to the first simulator it finds.
-	*/
-	function pushToVersion(version) {
-
-		findSimulators().then(function(simulators) {
-
-			if(simulators.length == 0) {
-				grunt.fail.warn('No simulators found');
-			}
-			else {
-
-				var target = null;
-
-				if (version == 'any') {
-					target = simulators[0];
-				}
-				else {
-					for (var i = 0; i < simulators.length; ++i) {
-						if (simulators[i].version == version) {
-							target = simulators[i];
-							break;
-						}
-					}
-				}
-
-				if (target != null) {
-					grunt.log.writeln('Launching simulator at version', target.version);
-					startSimulator(target).then(function(res) {
-						launchTheApp(res);
-					}, function(err) {
-						grunt.log.error(err);
-					});
-				}
-				else {
-					grunt.fail.warn('No simulator at version ' + version + ' found');
-				}
-			}
-		}).catch(function(e) {
-			grunt.log.writeln(e);
-		});
-	}
-
-	/**
-	 * Push the app (unit tests) to any Firefox runtime we can find.
-	 * This defaults a physical device, then falls back to a simulator.
-	 * It will launch the first simulator it finds.
-	*/
-	function pushToAnything() {
-
-		findDevices().then(forwardPorts).then(function(devices) {
-
-			if (devices.length == 0) {
-				grunt.log.writeln('No devices found');
-				pushToVersion('any');
-			}
-			else {
-				Promise.all(devices.map(function(device) {
-					return connectDevice(device)
-						.then(pushTheApp)
-						.then(launchTheApp);
-				})).then(quit);
-			}
-		})
-		.catch(function(err) {
-			grunt.log.error(err);
-		});
-	}
-
-	/**
-	 * Series of helper functions for launching apps
-	*/
-	function connectDevice(device) {
-		return connect(device.ports[0].port);
-	}
-	function pushTheApp(client) {
-		return pushApp(client, appPath);
-	}
-	function launchTheApp(res) {
-		return launchApp({
-			manifestURL: res.app.manifestURL,
-			client: res.client
-		});
-	}
-	function quit() {
-		grunt.log.writeln('ok bye');
-		// process.exit(0); // This would probably break grunt
-	}
-
-
-	/**
-	 * TODO ?
-	 * This mght not be necessary.
-	*/
-	function createQunitTest(name) {
-		grunt.log.writeln('TODO - make ' + name); //rmv
-		grunt.log.writeln('Do we really need this?'); //rmv
-	}
+  /**
+   * TODO ?
+   * This mght not be necessary.
+  */
+  function createQunitTest(name) {
+    grunt.log.writeln('TODO - make ' + name); //rmv
+    grunt.log.writeln('Do we really need this?'); //rmv
+  }
 };
---
+++ node-firefox-scripts/launch-firefoxos.js
@@ -0,0 +1,132 @@
+
+var fs = require('fs');  // ----- Node's Filesystem module
+var path = require('path');  // - Node's filepath module
+
+// A boat-load of node-firefox libraries
+var Promise = require('es6-promise').Promise;
+var findDevices = require('node-firefox-find-devices');
+var findSimulators = require('node-firefox-find-simulators');
+var startSimulator = require('node-firefox-start-simulator');
+var forwardPorts = require('node-firefox-forward-ports');
+var connect = require('node-firefox-connect');
+var launchApp = require('node-firefox-launch-app');
+var pushApp = require('push-app');
+
+// Where our app is located
+var appPath = path.join(__dirname, '../testrunner');
+
+
+/**
+ * Push the app (unit tests) to a Firefix OS simulator of a
+ * specific version. You will be notified if the specified
+ * runtime does not exist. You can install runtimes via the
+ * Firefox WebIDE.
+ * If the provided version is "any", it will simply connect
+ * to the first simulator it finds.
+*/
+function pushToVersion(version) {
+
+  findSimulators().then(function(simulators) {
+
+    if(simulators.length == 0) {
+      console.warn('No simulators found');
+    }
+    else {
+
+      var target = null;
+
+      if (version == 'any') {
+        target = simulators[0];
+      }
+      else {
+        for (var i = 0; i < simulators.length; ++i) {
+          if (simulators[i].version == version) {
+            target = simulators[i];
+            break;
+          }
+        }
+      }
+
+      if (target != null) {
+        console.log('Launching simulator at version', target.version);
+        startSimulator({ detached: true }).then(target).then(function(res) {
+          launchTheApp(res);
+        }, function(err) {
+          console.log(err);
+        });
+      }
+      else {
+        console.warn('No simulator at version ' + version + ' found');
+      }
+    }
+  }).catch(function(e) {
+    console.error(e);
+  });
+}
+
+/**
+ * Push the app (unit tests) to any Firefox runtime we can find.
+ * This defaults a physical device, then falls back to a simulator.
+ * It will launch the first simulator it finds.
+*/
+function pushToAnything() {
+  findDevices().then(forwardPorts).then(function(devices) {
+
+    if (devices.length == 0) {
+      console.warn('No devices found');
+      pushToVersion('any');
+    }
+    else {
+      Promise.all(devices.map(function(device) {
+        console.log(device);
+        return connectDevice(device)
+          .then(pushTheApp)
+          .then(launchTheApp);
+      })).then(quit);
+    }
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+}
+
+/**
+ * Series of helper functions for launching apps
+*/
+function connectDevice(device) {
+  return connect(device.ports[0].port);
+}
+
+function pushTheApp(client) {
+  return pushApp(client, appPath);
+}
+
+function launchTheApp(res) {
+  return launchApp({
+    manifestURL: res.app.manifestURL,
+    client: res.client
+  });
+}
+
+function quit() {
+  process.exit(0);
+}
+
+// Main
+if (process.argv.length == 2) {
+  pushToAnything();
+} else if (process.argv[2] == 'pushToAnything') {
+  pushToAnything();
+} else if (process.argv[2] == 'pushToVersion') {
+  if (process.argv[3] === 'true') {
+    console.warn('No simulator version specificed, defaulting to 3.0.');
+    pushToVersion('3.0')
+    return;
+  }
+
+  pushToVersion(process.argv[3])
+} else {
+  console.error("Second command line argument not understood, please use 'pushToAnything' or 'pushToVersion'.");
+  quit();
+}
+
