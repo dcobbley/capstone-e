02d801aefa8aca874b883e2448a45c4bdb8ca432
nlarsonX <daddadboy@gmail.com>
2015-05-26 18:31:36 -0700

    added settings.js

--- modules/settings.js
+++ modules/settings.js
@@ -1,61 +1,71 @@
  module.exports = function(Ffosbr) {
-   var sdcard = navigator.getDeviceStorage('sdcard');
-   var sdcard1 = navigator.getDeviceStorage('sdcard1');
+   var internalStorage = navigator.getDeviceStorage('sdcard');
+   var sdcard = navigator.getDeviceStorage('sdcard1');
    //var appSettings = navigator.getDeviceStorage('apps');
 
-   Ffosbr.prototype.settings = function() {
-     var DAYS_STARTING_SUNDAY = require('constants').DAYS_STARTING_SUNDAY;
+   // Ffosbr.prototype.settings = function() {
+   var DAYS_STARTING_SUNDAY = require('constants').DAYS_STARTING_SUNDAY;
 
+   {
+     backupTypes = {
+       'photos': false,
+       'videos': false,
+       'contacts': false,
+       'text': false
+     };
+   }
 
-     //function BackupItems() {
-     var backupTypes = document.getElementById('SelectItems');
+   {
+     backupTimes = {
+       'id': null,
+       'registeredTimer': false,
+       'repeat': false,
+       'daily': false,
+       'weekly': false,
+       'monthly': false
+     };
 
-     if (backupTypes) {
-       var userItems = {
-         photos: backupTypes[0].value,
-         videos: backupTypes[1].value,
-         contacts: backupTypes[2].value,
-         textMsgs: backupTypes[3].value
-       };
-       //return userItems;
+   }
+
+   Ffosbr.prototype.BackupItems = function(key, value) {
+     if (key == 'photos') {
+       backupTypes.photos = value;
      }
-     //}
+     if (key == 'videos') {
+       backupTypes.videos = value;
+     }
+     if (key == 'contacts') {
+       backupTypes.contacts = value;
+     }
+     if (key == 'text') {
+       backupTypes.text = value;
+     }
+     return backupTypes;
 
-     //BackupItems();
-     /*
-        function Timer(opts) {
-          opts = opts || {};
-          var now = new Date();
-          var defaults = {
-            id: null,
-            registeredTimer: {},
-            repeat: {},
-            hour: now.getHours(),
-            minute: now.getMinutes(),
-            label: ''
-          };
-
-        }
-
-        var file = new Blob(['This is a settings file.'], {
-          type: 'text/plain'
-        });
-        var request = appSettings.addNamed(file, 'settings.txt');
-
-        request.onsuccess = function() {
-          var name = this.result;
-          console.log('File ' + name + 'successfully wrote to app storage');
-
-        };
-
-        request.onerror = function() {
-          console.warn('Unable to write the file: ' + this.error);
-        };
-
-
-
-     */
-
-     Ffosbr.settings = new Settings();
    };
+
+   Ffosbr.prototype.BackupTimer = function(key, value) {
+     if (key == 'id') {
+       backupTimes.id = value;
+     }
+     if (key == 'registeredTimer') {
+       backupTimes.registeredTimer = value;
+     }
+     if (key == 'repeat') {
+       backupTimes.repeat = value;
+     }
+     if (key == 'daily') {
+       backupTimes.daily = value;
+     }
+     if (key == 'weekly') {
+       backupTimes.weekly = value;
+     }
+     if (key == 'monthly') {
+       backupTimes.monthly = value;
+     }
+     return backupTimes;
+   };
+
+
+   // Ffosbr.settings = new Settings();
  };
