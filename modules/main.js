68a48dbba4acd55a4a6945da8a1d8a85c6cb0de7
drnida <rnida@mac.local>
2015-06-13 15:26:14 -0700

    ./photos breaks the tests

--- modules/main.js
+++ modules/main.js
@@ -5,7 +5,7 @@
     /* Import modules */
     this.utils = require('./utils');
     this.media = require('./media');
-    this.photos = require('./photos');
+    //this.photos = require('./photos');
   };
 
   window.ffosbr = new Ffosbr();
