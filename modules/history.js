67d90145cc3caaa896b5da81ace3365895c2bcb3
Ryan Bernstein <LBRyanLB@comcast.net>
2015-07-09 23:41:23 -0700

    CTE-46 Added a HistoryEntry prototype with (thus far untested) get(), set(), validateAll(), and validate() methods.

--- modules/history.js
+++ modules/history.js
@@ -1,3 +1,78 @@
+function HistoryEntry() {
+  var lastUpdated = new Date();
+  var backupSize = 0;
+}
+
+function HistoryEntry(lastUpdatedVal, backupSizeVal) {
+  var lastUpdated = new Date();
+  var backupSize = 0;
+
+  if (typeof lastUpdatedVal === 'date') {
+    lastUpdated = lastUpdatedVal;
+  }
+  if (typeof backupSizeVal === 'number') {
+    backupSize = backupSizeVal;
+  }
+}
+
+HistoryEntry.prototype.validateAll = function(potentialHistoryEntry) {
+  var hist = null;
+  // If potentialHistoryEntry was null, validate this object
+  if (typeof potentialHistoryEntry === 'undefined') {
+    hist = this;
+  } else if (typeof potentialHistoryEntry === object) {
+    hist = potentialHistoryEntry;
+  } else {
+    return false;
+  }
+  
+  // Validate individual fields
+  if (!this.validate('lastUpdated', hist['lastUpdated'])) {
+    return false;
+  }
+  if (!this.validate('backupSize', hist['backupSize'])) {
+    return false;
+  }
+
+  // Ensure that we don't have extra fields
+  if (!Object.keys(this).length == 2) {
+    return false;
+  }
+
+  return true;
+};
+
+HistoryEntry.prototype.validate = function(field, value) {
+  if (field === 'lastUpdated') {
+    return Date.parse(value) != NaN;
+  }
+  if (field === 'backupSize') {
+    return typeof value === 'number' && value >= 0;
+  }
+
+  return false;
+};
+
+HistoryEntry.prototype.set = function(field, value) {
+  // If we were passed an object, try and set its fields
+  if (typeof field === 'object' && this.validateAll(field)) {
+    this.lastUpdated = field['lastUpdated'];
+    this.backupSize = field['backupSize'];
+  } else if (typeof field === 'string' && this.validate(field, value)) {
+    this[field] = value;
+  }
+};
+
+HistoryEntry.prototype.get = function(field) {
+  // If field is undefined, return the whole object
+  if (typeof field === 'undefined') {
+    return this;
+  }
+
+  // Return the given field
+  return this[field];
+}
+
 function History() {
   // Default values, which should be overwritten if we had a valid
   // history on disk
