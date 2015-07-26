af8d5f4b21c2e9d6dd7b30a58d8765c501b54dae
Ryan Bernstein <LBRyanLB@comcast.net>
2015-07-25 18:00:51 -0700

    Add wrappers for Photos, Music, and Videos. This commit is Not Very Exciting.

---
+++ modules/photos.js
@@ -0,0 +1,40 @@
+/**
+ * Wrapper module that allows Photos to be backed up using standard backup/restore/clean
+ * methods.
+ */
+function Photos() {}
+
+/**
+ * @description Photos initializer.
+ */
+Photos.prototype.initialize = function() {};
+
+/**
+ * @description Wrapper that calls ffosbr.backup('pictures').
+ *  Oncomplete callback is invoked upon completion; if an error occurred,
+ *  it will be passed as the first parameter to the callback.
+ * @param {callback} oncomplete
+ */
+Photos.prototype.backup = function(oncomplete) {
+  ffosbr.backup('pictures', oncomplete);
+};
+
+/**
+ * @description Wrapper for ffosbr.restore('pictures').
+ *  Oncomplete callback is invoked upon completion.
+ * @param {callback} oncomplete
+ */
+Photos.prototype.restore = function(oncomplete) {
+  ffosbr.restore('pictures', oncomplete);
+};
+
+/**
+ * @description Wrapper for ffosbr.clean('pictures').
+ *  Oncomplete callback is invoked upon completion.
+ * @param {callback} oncomplete
+ */
+Photos.prototype.clean = function(oncomplete) {
+  ffosbr.clean('pictures', oncomplete);
+};
+
+module.exports = new Photos();
