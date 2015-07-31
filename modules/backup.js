ead3fc1bf4f30ddfcb1b24fcd1e073d30d4f97e0
Ruben Niculcea <ruben.niculcea@gmail.com>
2015-07-30 19:39:41 -0700

    Refactor backup clean and restore into media.

--- modules/backup.js
+++ modules/backup.js
@@ -1,33 +1,10 @@
 /**
  * @access public
- * @description Saves specified file type to external storage
- *   device. Callback is invoked upon completion. If an error
- *   occurred, it will be passed as the first parameter to
- *   the callback, "oncomplete".
- * @param {string} type
+ * @description Backups every data type set as true in settings
  * @param {callback} oncomplete
- * @throws On invalid data type
  */
-var backup = function(type, oncomplete) {
+var backup = function(oncomplete) {
 
-  var paths = ffosbr.settings.getBackupDirectoryPaths();
-
-  if (paths[type] === undefined) {
-    throw new Error('Invalid data type. Cannot restore type ' + type);
-  }
-
-  ffosbr.media.get(type === 'photos' ? 'pictures' : type, function(file) {
-    if (!file) {
-      return;
-    }
-
-    var fn = file.name;
-    fn = fn.substr(fn.lastIndexOf('/') + 1, fn.length);
-    var dest = paths[type] + fn + '~';
-    ffosbr.media.put('sdcard1', file, dest, function() {
-      // Report progress?
-    });
-  }, oncomplete);
 };
 
 // Defines Ffosbr backup
--- modules/clean.js
+++ modules/clean.js
@@ -1,32 +1,9 @@
 /**
  * @access public
- * @description Deletes specified file types from external storage
- *   device. Callback is invoked upon completion. If an error
- *   occurred, it will be passed as the first parameter to
- *   the callback, "oncomplete".
- * @param {string} type
+ * @description Deletes every data type set as true in settings
  * @param {callback} oncomplete
- * @throws On invalid data type
  */
-var clean = function(type, oncomplete) {
-  var paths = ffosbr.settings.getBackupDirectoryPaths();
-
-  if (paths[type] === undefined) {
-    throw new Error('Invalid data type. Cannot clean type ' + type);
-  }
-
-  ffosbr.media.get('sdcard1', paths[type], function(file) {
-    if (!file) {
-      return;
-    }
-
-    var filename = paths[type] + file.name;
-    window.ffosbr.media.remove(file.name, function(error) {
-      if (error) {
-        throw error;
-      }
-    });
-  }, oncomplete);
+var clean = function(oncomplete) {
 
 };
 
