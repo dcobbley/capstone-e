54898cb3922bd20980fb5f48bdf91ca1841761a7
nlarsonX <daddadboy@gmail.com>
2015-08-10 09:52:00 -0700

    fixed local storage and sd copying nonsense

--- modules/systemSettings.js
+++ modules/systemSettings.js
@@ -40,9 +40,7 @@ function SystemSettings() {
     'icc.inputTextTimeout': null
   };
 
-
 }
-
 
 /**
  *@description SystemSettings Constructor
@@ -59,62 +57,6 @@ SystemSettings.prototype.initialize = function() {
 
 
 /*
- *@Description: restores settings to the device from the SD card.
- *  calls media.get to get data from Sd card.
- *  calls writeToLocalStorage to save instance of systemsettings
- *  calls writeToDevice to transfer lcoal storage object to the device's settings
- *
- */
-
-SystemSettings.prototype.restore = function() {
-  var that = this;
-  var path = '/sdcard1/' + ffosbr.settings.backupPaths.systemsettings;
-
-
-  /*
-   *@Description: helper used in media.get.
-   *  Reads the passed in file (from media.get) and saves it to
-   *   the systemsettings.
-   */
-
-  var copyToSettings = function(file) {
-    var filereader = new FileReader();
-
-    settingsJSON = filereader.readAsText(file);
-
-    filereader.onloadend = function(fileContents, error) {
-
-      if (!error) {
-        try {
-          that.systemsettings = this.result;
-        } catch (e) {
-          console.error(e);
-        }
-
-      } else {
-        console.error(error);
-      }
-
-    };
-
-    filereader.onerror = function(error) {
-      console.error('Failure: ' + error.name);
-    };
-
-  };
-
-  var oncomplete = function() {
-    console.log('successful restore');
-  };
-
-
-  ffosbr.media.get('sdcard1', path, copyToSettings, oncomplete);
-  this.writeToLocalStorage();
-  this.writeSettingsToDevice();
-  console.log('system settings successfully restored');
-
-};
-/*
  *@Description: removes the systemsettings file from the sd card
  *  calls media.remove
  *
@@ -128,6 +70,7 @@ SystemSettings.prototype.clean = function() {
 
 };
 
+
 /*
  *@Description: backup the systemsettings object to SD card.
  *
@@ -138,34 +81,93 @@ SystemSettings.prototype.backup = function() {
   var that = this;
   var path = ffosbr.settings.backupPaths.systemsettings + '/systemSettings.json';
 
-  //this.clean();
-
   var settings = this.loadFromDevice();
 
   var settingsToWrite = new File([JSON.stringify(settings)], 'systemSettings.json', {
     type: 'text/json'
   });
 
-  console.log(settingsToWrite);
-
   ffosbr.media.put('sdcard1', settingsToWrite, path);
-
 };
 
-/**
- *@description: helper function to be used in update field.
- *  Needed to allow access to this.systemsettings
+
+/*
+ *@Description: restores settings to the device from the SD card.
+ *  calls media.get to get data from Sd card.
+ *  calls writeToLocalStorage to save instance of systemsettings
+ *  calls writeToDevice to transfer lcoal storage object to the device's settings
  *
- *@params: pass in the name of the setting (field) and 
- *  the result from the lock.get (settingResult).
  */
 
-SystemSettings.prototype.onsuccess = function(field, settingResult) {
-  this.systemsettings[field] = settingResult[field];
+SystemSettings.prototype.restore = function() {
+  var path = '/sdcard1/' + ffosbr.settings.backupPaths.systemsettings;
 
+  ffosbr.media.get('sdcard1', path, copyToSettings, oncomplete);
+  this.writeSettingsToDevice(JSON.stringify(this.systemsettings));
+  console.log('system settings successfully restored');
 
 };
 
+
+/*
+ *@Description: helper used in media.get.
+ *  Reads the passed in file (from media.get) and saves it to
+ *   the systemsettings.
+ */
+
+var copyToSettings = function(file) {
+  var that = this;
+  var filereader = new FileReader();
+
+  settingsJSON = filereader.readAsText(file);
+
+  filereader.onloadend = function(fileContents, error) {
+
+    if (!error) {
+      try {
+        that.systemsettings = this.result;
+      } catch (e) {
+        console.error(e);
+      }
+
+    } else {
+      console.error(error);
+    }
+
+  };
+
+  filereader.onerror = function(error) {
+    console.error('Failure: ' + error.name);
+  };
+
+};
+
+/*
+ *@Description: helper function used in media.get
+ *
+ *
+ */
+
+var oncomplete = function() {
+  console.log('successful restore');
+};
+
+
+
+/**
+ *@description: load System Settings from device storage
+ * save these settings to a JSON object
+ *@access: public, requires 'settings' in manifest
+ */
+SystemSettings.prototype.loadFromDevice = function() {
+
+  for (var field in this.systemsettings) {
+    ffosbr.systemSettings.updateField(field);
+  }
+
+  return this.systemsettings;
+
+};
 
 /**
  *@description: helper function to be used in load settings. 
@@ -192,33 +194,49 @@ SystemSettings.prototype.updateField = function(field) {
 
 };
 
-
-
 /**
- *@description: load System Settings from device storage
- * save these settings to a JSON object
- *@access: public, requires 'settings' in manifest
+ *@description: helper function to be used in update field.
+ *  Needed to allow access to this.systemsettings
+ *
+ *@params: pass in the name of the setting (field) and 
+ *  the result from the lock.get (settingResult).
  */
-SystemSettings.prototype.loadFromDevice = function() {
 
-  for (var field in this.systemsettings) {
-    ffosbr.systemSettings.updateField(field);
-  }
+SystemSettings.prototype.onsuccess = function(field, settingResult) {
+  this.systemsettings[field] = settingResult[field];
 
-  return this.systemsettings;
 
 };
 
 
 /**
- *@description: After the settings have been loaded, call this function
- *  to write to local storage.
+ *@description: This function first loads our backed up copy of system settings.
+ *  We then update the system settings based on what our backed up copy has stored.
+ *
  *
  */
 
-SystemSettings.prototype.writeToLocalStorage = function() {
-  localStorage.setItem('ffosbrSystemSettings', JSON.stringify(this.systemsettings));
-  console.log(this.systemsettings);
+
+SystemSettings.prototype.writeSettingsToDevice = function(settings) {
+
+  var retrievedSettings = settings;
+
+  if (retrievedSettings !== null) {
+    try {
+      retrievedSettings = JSON.parse(retrievedSettings);
+    } catch (err) {
+      console.log('cannot fetch object');
+      localStorage.setItem('ffosbrSystemSettings', null);
+      throw new Error('Fetched an invalid options object from local storage');
+    }
+  } else {
+    console.log('retrievedSettings is null');
+  }
+
+  for (var field in retrievedSettings) {
+    ffosbr.systemSettings.setField(field, retrievedSettings[field]);
+  }
+
 };
 
 
@@ -246,61 +264,5 @@ SystemSettings.prototype.setField = function(settingName, settingValue) {
     console.log('failed to update setting');
   };
 };
-
-/**
- *@description: This function first loads our backed up copy of system settings.
- *  We then update the system settings based on what our backed up copy has stored.
- *
- *
- */
-
-
-SystemSettings.prototype.writeSettingsToDevice = function() {
-
-  var retrievedSettings = localStorage.getItem('ffosbrSystemSettings');
-
-  if (retrievedSettings !== null) {
-    try {
-      retrievedSettings = JSON.parse(retrievedSettings);
-    } catch (err) {
-      console.log('cannot fetch object');
-      localStorage.setItem('ffosbrSystemSettings', null);
-      throw new Error('Fetched an invalid options object from local storage');
-    }
-  } else {
-    console.log('retrievedSettings is null');
-  }
-
-  for (var field in retrievedSettings) {
-    ffosbr.systemSettings.setField(field, retrievedSettings[field]);
-  }
-
-};
-
-/**
- *@Description: this function handles changes in system settings.
- *  When a setting is changed, this function should be called to update our local version of system settings.
- *  This way we can consistently have the most up to date version of system settings.
- *
- *  APPLICATION SIDE MUST HAVE A LISTENER FOR settingchange EVENT!
- *  navigator.mozSettings.onsettingchange = function(event) {
- *    ffosbr.systemSettings.updateOnSettingChange(event);
- *
- *   }
- *  then pass that event to this function.
- *
- *
- * NEEDS TO BE TESTED 
- */
-
-SystemSettings.prototype.updateOnSettingChange = function(event) {
-  var settingName = event.settingName;
-  var settingValue = event.settingValue;
-
-  this.systemsettings[settingName] = settingValue;
-  ffosbr.systemSettings.writeToLocalStorage();
-
-};
-
 
 module.exports = new SystemSettings();
